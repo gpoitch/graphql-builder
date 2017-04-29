f811c023e3d5002e573a288448e984a40148593b
Garth Poitras <garth22@gmail.com>
2017-04-29 12:33:13 -0400

    code cleanup

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const FragmentMap = {}
 const REGEX_FRAGMENT_SPREAD = /\.\.\.(\w+)/g
 const REGEX_FRAGMENT_INLINE = /^\s*on/
-const REGEX_FRAGMENT_NAME = /fragment\s*([_A-Za-z][_0-9A-Za-z]*)\s/
+const REGEX_FRAGMENT_NAME = /fragment\s*(\w+)/
+const FragmentMap = {}
 
 function extractFragmentSpreads (query) {
   const fragments = []
-  let match = REGEX_FRAGMENT_SPREAD.exec(query)
-  while (match != null) {
+  let match
+  while ((match = REGEX_FRAGMENT_SPREAD.exec(query)) !== null) {
     const fragmentName = match[1]
     if (!REGEX_FRAGMENT_INLINE.test(fragmentName)) {
       fragments.push(fragmentName)
     }
-    match = REGEX_FRAGMENT_SPREAD.exec(query)
   }
   return fragments
 }
@@ -22,49 +21,43 @@ function extractFragmentName (definition) {
 }
 
 function collectFragments (fragments, collection) {
-  for (let i = 0; i < fragments.length; i++) {
+  for (let i = 0, len = fragments.length; i < len; i++) {
     const name = fragments[i]
     const childFragments = FragmentMap[name].fragments
     if (collection.indexOf(name) === -1) {
       collection.push(name)
       childFragments.length && collectFragments(childFragments, collection)
-    } else {
-      break
     }
   }
   return collection
 }
 
 function reduceFragments (fragments) {
-  return collectFragments(fragments, []).reduce((string, name) => {
-    string += '\n' + FragmentMap[name].definition
-    return string
-  }, '')
+  return collectFragments(fragments, []).reduce((string, name) => (string += '\n' + FragmentMap[name].definition), '')
 }
 
 function createOperation (type, opts) {
-  let operationName, operationDefinition
   if (typeof opts === 'string') {
-    operationDefinition = opts
-  } else {
-    const { name, variables, definition } = opts
-    operationName = name || ''
-    operationDefinition = name ? `${type} ${name} ` : ''
-    if (variables) {
-      if (!operationName) {
-        operationDefinition = `${type} `
-      }
-      const params = []
-      for (let name in variables) {
-        params.push(`$${name}: ${variables[name]}`)
-      }
-      operationDefinition += `(${params.join(', ')}) `
-    }
-
-    operationDefinition += definition
+    return createOperation(type, { definition: opts })
   }
 
-  const fragments = extractFragmentSpreads(operationDefinition)
+  const { name, variables, definition } = opts
+  const operationName = name || ''
+  let operationDefinition = name ? `${type} ${name} ` : ''
+  if (variables) {
+    if (!operationName) {
+      operationDefinition = `${type} `
+    }
+    const params = []
+    for (let name in variables) {
+      params.push(`$${name}: ${variables[name]}`)
+    }
+    operationDefinition += `(${params.join(', ')}) `
+  }
+
+  operationDefinition += definition
+
+  const fragments = extractFragmentSpreads(definition)
   const doc = `${operationDefinition}${reduceFragments(fragments)}`
 
   const operationObj = {
@@ -79,32 +72,32 @@ function createOperation (type, opts) {
   return operationObj
 }
 
-export function fragment (opts) {
-  let fragmentName, fragmentDefinition
+function fragment (opts) {
   if (typeof opts === 'string') {
-    fragmentName = extractFragmentName(opts)
-    fragmentDefinition = opts
-  } else {
-    const { name, on, definition } = opts
-    fragmentName = name || (on && `Fragment${on}`) || extractFragmentName(definition)
-    fragmentDefinition = on ? `fragment ${fragmentName} on ${on} ${definition}` : definition
+    return fragment({ definition: opts })
   }
 
-  const fragment = {
+  const { name, on, definition } = opts
+  const fragmentName = name || (on && `Fragment${on}`) || extractFragmentName(definition)
+  const fragmentDefinition = on ? `fragment ${fragmentName} on ${on} ${definition}` : definition
+
+  const fragmentObj = {
     name: fragmentName,
     definition: fragmentDefinition,
     fragments: extractFragmentSpreads(fragmentDefinition),
     toString: () => `...${fragmentName}`
   }
 
-  FragmentMap[fragmentName] = fragment
-  return fragment
+  FragmentMap[fragmentName] = fragmentObj
+  return fragmentObj
 }
 
-export function query (opts) {
+function query (opts) {
   return createOperation('query', opts)
 }
 
-export function mutation (opts) {
+function mutation (opts) {
   return createOperation('mutation', opts)
 }
+
+export { fragment, query, mutation }
